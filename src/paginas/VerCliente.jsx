ab0a3ba34a4d0a8dea0707fd71f40b1a70c7e310
unknown <mario120gdl@gmail.com>
2022-04-15 15:20:59 -0500

    Proyecto Finalizado

--- src/paginas/VerCliente.jsx
+++ src/paginas/VerCliente.jsx
@@ -5,14 +5,14 @@ import Spinner from "../components/Spinner";
 const VerCliente = () => {
   const [cliente, setCliente] = useState({});
   const [cargando, setCargando] = useState(false);
-  
+
   const { id } = useParams();
 
   useEffect(() => {
     const obtenerClienteAPI = async () => {
       try {
         setCargando(true);
-        const url = `http://localhost:4000/clientes/${id}`;
+        const url = `${import.meta.env.VITE_API_URL}/${id}`;
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
         setCliente(resultado);
